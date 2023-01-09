b16daca79d8a3d62686e681535a6e59cd1302b31
Albi Mudakar Nasyabi <albimdkr@gmail.com>
2023-01-10 04:43:10 +0700

    tinggal config landing page

---
+++ dbfile.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2023 at 12:49 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbfile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `permissions` varchar(10) NOT NULL DEFAULT '',
+  `homedir` varchar(1000) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `akey` varchar(255) NOT NULL DEFAULT '',
+  `usage` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `permissions`, `homedir`, `email`, `akey`, `usage`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500', 'rwu', '', '', '', NULL),
+(2, 'guest', '', 'r', '', '', '', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `akey` (`akey`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ include/views/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" >
+	<meta name="description" content="file gator" >
+	<title>KELOMPOK 8</title>
+	<link rel="shortcut icon" href="./include/views/img/favicon.ico" />
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="./include/jquery/jquery-1.8.0.min.js"></script>
+	
+	<!-- Blueimp -->
+	<link rel="stylesheet" href="./include/blueimp/css/jquery.fileupload-ui.css">
+	<link rel="stylesheet" href="./include/blueimp/css/style.css">
+	<script src="./include/blueimp/js/vendor/jquery.ui.widget.js"></script>
+	<!-- The Templates plugin is included to render the upload/download listings -->
+	<script src="./include/blueimp/js/tmpl.min.js"></script>
+	<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+	<script src="./include/blueimp/js/jquery.iframe-transport.js"></script>
+	<!-- The basic File Upload plugin -->
+	<script src="./include/blueimp/js/jquery.fileupload.js"></script>
+	<!-- The File Upload file processing plugin -->
+	<script src="./include/blueimp/js/jquery.fileupload-fp.js"></script>
+	<!-- The File Upload user interface plugin -->
+	<script src="./include/blueimp/js/jquery.fileupload-ui.js"></script>
+	<!-- The localization script -->
+	<script src="./include/blueimp/js/locale.js"></script>
+	
+	<!-- Lightbox2 -->
+	<?php if(gatorconf::get('use_lightbox_gallery')):?>
+	<script type="text/javascript" src="./include/lightbox/lightbox.js"></script>
+	<link rel="stylesheet" type="text/css" href="./include/lightbox/lightbox.css">
+	<?php endif;?>
+	
+	<!-- Foundation3 -->
+	<link rel="stylesheet" href="./include/foundation/stylesheets/foundation.css">
+	<script src="./include/foundation/javascripts/jquery.foundation.buttons.js"></script>
+	<script src="./include/foundation/javascripts/jquery.foundation.reveal.js"></script>
+	
+	<!-- FileGator styles -->
+	<link rel="stylesheet" href="./include/views/style.css">
+	<!--[if lte IE 7]>
+	<link rel="stylesheet" href="./include/views/style_ie7fix.css" />
+	<![endif]-->
+
+</head>		
